d42eae82a3986a29009b3666b892ad50eb5a80aa
Philip Cheng <philipcheng@10-248-20-45.wireless.oregonstate.edu>
2016-05-30 11:21:18 -0700

    fix __init__

--- tensorpack/tfutils/__init__.py
+++ tensorpack/tfutils/__init__.py
@@ -2,19 +2,10 @@
 #  File: __init__.py
 #  Author: Yuxin Wu <ppwwyyxx@gmail.com>
 
-from pkgutil import walk_packages
-import os
-
-def _global_import(name):
-    p = __import__(name, globals(), None, level=1)
-    lst = p.__all__ if '__all__' in dir(p) else dir(p)
-    if name in ['common', 'argscope']:
-        del globals()[name]
-    for k in lst:
-        globals()[k] = p.__dict__[k]
-
-_global_import('sessinit')
-_global_import('common')
-_global_import('gradproc')
-_global_import('argscope')
-
+from tensorpack.tfutils.argscope import *
+from tensorpack.tfutils.common import *
+from tensorpack.tfutils.gradproc import *
+from tensorpack.tfutils.modelutils import *
+from tensorpack.tfutils.sessinit import *
+from tensorpack.tfutils.summary import *
+from tensorpack.tfutils.symbolic_functions import *
